09d0fbe0092c2302fefe5bf5c846f68e9ed2d284
Thinkless <cxhfocus1991@gmail.com>
2017-05-05 08:51:55 +0800

    Create para_passbyreference_value.py

---
+++ para_passbyreference_value.py
@@ -0,0 +1,16 @@
+def try_to_change_list_content(the_list):
+    print('got', the_list)
+    the_list.append('study')
+    print('change', the_list)
+
+org_list = ['cheng', 'xin', 'hua']
+
+
+def try_to_change_list_reference(the_list):
+    print('got', the_list)
+    the_list = ['cheng', 'xin', 'hua', 'study', 'python']
+    print('change', the_list)
+
+print('bef_list', org_list)
+try_to_change_list_reference(org_list)
+print('after_list', org_list)
