f6e9912db6d0e00219062f9fb8fbb5fcbaa8f143
Thinkless <cxhfocus1991@gmail.com>
2017-05-05 09:56:00 +0800

    Create primes.py

---
+++ primes.py
@@ -0,0 +1,27 @@
+def odd():
+    n = 1
+    while True:
+        n += 2
+        yield n
+        
+        
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+
+def primes():
+    yield 2
+    it = odd()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+        
+for s in primes():
+    if s < 50:
+        print(s)
+    else:
+        break
+        
+        
+  
